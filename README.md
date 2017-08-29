420017b68e972cbad82164746e2486d0869d45bc
Ge Yang <yangge1987@gmail.com>
2017-08-29 00:44:06 -0500

    add functional notations code

---
+++ README.md
@@ -0,0 +1,37 @@
+# `functional_notations`, a utility abusing Python @ symbol for the greater good
+
+## Todo
+- [ ] Write examples and glorify `functional_notations`'s abusive usage
+
+### Done
+
+    
+## Installation
+```bash
+pip install at
+```
+
+## Usage
+
+```python
+```
+
+## To Develop
+
+```bash
+git clone https://github.com/episodeyang/functional_notations.git
+cd functional_notations
+make dev
+```
+
+To test, run
+```bash
+make test
+```
+
+This `make dev` command should build the wheel and install it in your current python environment. Take a look at the [./Makefile](./Makefile) for details.
+
+**To publish**, first update the version number, then do:
+```bash
+make publish
+```
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name="functional_notations",
+      description="An Decorator Library that allows @ notation for functions",
+      long_description="Abuses the @ notation of python3.5, allowing you to chain functions with the @ operator.",
+      version="0.0.0",
+      url="https://github.com/episodeyang/f",
+      author="Ge Yang",
+      author_email="yangge1987@gmail.com",
+      license=None,
+      keywords=["functional notations", "decorator", "functional", "functional programming"],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "Programming Language :: Python :: 3"
+      ],
+      packages=["functional_notations"],
+      install_requires=[]
+      )
